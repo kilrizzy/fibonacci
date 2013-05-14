b6887de0dabc5d05859fb36f08f0e341948157e8
Jeff Kilroy <mail@jeffkilroy.com>
2013-05-14 17:09:53 -0400

    Generator function + settings added

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Functions to display a fibonacci sequence and view a counter of sequences
 
 TTD
 ===
-Run sequencer from plugin file
-Create function doc
+- Function documentation
+- Start / Stop / Restart on generator
+- Add to github pages
--- js/jquery.fibonacci.js
+++ js/jquery.fibonacci.js
@@ -4,28 +4,49 @@
     	var result = new Object();
         this.each( function() {
             //value = sequence;
-            result.value = fibonacciValueGet(sequence);
-            result.ratio = fibonacciRatioGet(sequence);
+            result = fibonacciGets(sequence);
         });
         return result;
     }
 
-    $.fn.fibonacciGenerator = function() {
+    $.fn.fibonacciGenerator = function(options) {
+
+    	var settings = $.extend({
+            start        : 1,
+            end          : 99,
+            loop         : true,
+            speed        : 500,
+            group        : true,
+        }, options);
 
         this.each( function() {
+        	this.currentsequence = settings.start;
+        	this.display = '';
+        	fibonacciRunGenerator(this,settings);
+        	//this.timer = setTimeout(function(){ console.log(this); }, settings.speed);
         	//Create timer and run generater.
         	//Setup generator commands (pause + next + back + restart)
         });
 
     }
-    //
+    //Get the fibonacci data from sequence number
+    function fibonacciGets(s){
+    	var result = new Object();
+    	result.sequence = s;
+    	result.value = fibonacciValueGet(s);
+        result.ratio = fibonacciRatioGet(s);
+		return result;
+    }
+    //Get the fibonacci value of the given sequence
     function fibonacciValueGet(s){
     	val = 0;
     	if(!isNaN(s)){
+
 	  		val=Math.round((Math.pow((1+Math.sqrt(5))/2,s)-Math.pow((1-Math.sqrt(5))/2,s))/Math.sqrt(5));
 		}
 		return val;
     }
+    //Get the ratio of the given sequence (previous / current). Will get closer to 1.618 as the numbers get higher
     function fibonacciRatioGet(s){
     	var ratio = 0;
     	if(s > 1){
@@ -34,5 +55,38 @@
     		ratio = val1/val2;
     	}
     	return ratio;
+    }
+    //fibonacciInitializeGenerator 
+    function fibonacciRunGenerator(element,settings){
+    	//var currentsequence = fibonacciValueGet;
+    	var sequenceData = fibonacciGets(element.currentsequence);
+    	console.log(sequenceData);
+    	//display
+    	var display = '<ul>';
+    	display += '<li><strong>Sequence:</strong> '+sequenceData.sequence+'</li>';
+    	display += '<li><strong>Value:</strong> '+sequenceData.value+'</li>';
+    	display += '<li><strong>Ratio:</strong> '+sequenceData.ratio+'</li>';
+    	display += '</ul>';
+    	if(settings.group){
+    		element.display += display;
+    	}else{
+    		element.display = display;
+    	}
+    	$(element).html(element.display);
+    	//Continue?
+    	var continueTimer = true;
+    	if(element.currentsequence < settings.end){
+	    	element.currentsequence++;
+    	}else if(settings.loop){
+    		element.display = '';
+    		element.currentsequence = 1;
+    	}else{
+    		continueTimer = false;
+    	}
+    	//
+    	if(continueTimer){
+	    	element.timer = setTimeout(function(){ fibonacciRunGenerator(element,settings); }, settings.speed);
+    	}
+    	
     }
 }(jQuery));
